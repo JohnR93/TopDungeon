1eef0b327e1b2238658cf4c5bd671c1d2ff79855
JohnR93 <31391361+JohnR93@users.noreply.github.com>
2022-03-16 19:14:09 -0300

    Starting FloatingTextManager script

---
+++ Assets/Scripts/FloatingTextManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingTextManager : MonoBehaviour
+{
+    public GameObject textContainer;
+    public GameObject textPrefab;
+}
